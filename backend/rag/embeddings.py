2f008f071e7e2a17f9c1381bec65c3e26b3aaafa
GHK73 <22je0371@iitism.ac.in>
2026-04-06 22:28:49 +0530

    linalg

--- backend/rag/embeddings.py
+++ backend/rag/embeddings.py
@@ -8,7 +8,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def create_vector_store(chunks):
     embeddings = model.encode(chunks)
-    embeddings = embeddings / np.linlag.norm(embeddings, axis = 1,keepdims = True)
+    embeddings = embeddings / np.linalg.norm(embeddings, axis = 1,keepdims = True)
     dimension = embeddings.shape[1]
     index = faiss.IndexFlatIP(dimension)
     index.add(embeddings)
