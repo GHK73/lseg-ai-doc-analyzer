3f728fe6b1c5bba053e84956b47465e78aed83b0
GHK73 <22je0371@iitism.ac.in>
2026-04-06 21:41:01 +0530

    Readme

---
+++ backend/rag/qa.py
@@ -0,0 +1,20 @@
+from config import client
+
+
+def generate_answer(query, context):
+    prompt = f"""
+    Answer the question based only on the context below. 
+    If the answer is not present, say "Not found in the document". 
+
+    Context: {context}
+
+    Question: {query}
+    """
+
+    response = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {"role":"user","content":prompt}
+        ]
+    )
+    return response.choices[0].message.content
